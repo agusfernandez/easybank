b366f3c50338f70261b1a3454412970ed5fd4710
agusfernandez <agustina.fer91@gmail.com>
2021-11-23 23:24:46 -0300

    hamburger menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,17 @@
-console.log("hola");
+const btnhamburger=document.querySelector('#btn-hamburger');
+const header = document.querySelector('.header');
+const overlay= document.querySelector('.overlay__layer');
+
+btnhamburger.addEventListener('click', function(){
+    console.log("hamurguesa aca");
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        overlay.classList.remove ('fade-in');
+        overlay.classList.add ('fade-out');
+
+    } else {
+        header.classList.add('open');
+        overlay.classList.remove ('fade-out');
+        overlay.classList.add ('fade-in');
+    }
+})
