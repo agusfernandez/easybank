6519b6bb3cdb020371d3f26c3cbd185bfd6d449a
agusfernandez <agustina.fer91@gmail.com>
2021-11-24 23:19:10 -0300

    finish menu mobile

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,26 @@
 const btnhamburger=document.querySelector('#btn-hamburger');
+const body= document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay= document.querySelector('.overlay__layer');
+const fadeElements = document.querySelectorAll('.in__fade');
 
 btnhamburger.addEventListener('click', function(){
-    console.log("hamurguesa aca");
+    console.log("hamburguesa aca");
     if(header.classList.contains('open')){
         header.classList.remove('open');
-        overlay.classList.remove ('fade-in');
-        overlay.classList.add ('fade-out');
+        body.classList.remove('noscroll')
+        fadeElements.forEach(function(element){
+            element.classList.remove ('fade-in');
+            element.classList.add ('fade-out');
+        });
+ 
 
     } else {
         header.classList.add('open');
-        overlay.classList.remove ('fade-out');
-        overlay.classList.add ('fade-in');
+        body.classList.add('noscroll')
+        fadeElements.forEach(function(element){
+            element.classList.remove ('fade-out');
+            element.classList.add ('fade-in');
+        });
     }
 })
